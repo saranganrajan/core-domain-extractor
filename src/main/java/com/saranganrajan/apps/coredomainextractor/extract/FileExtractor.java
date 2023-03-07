b1eadc2fe57ea8a54e3d1bc86783a734f6cb8c4f
Sarangapani Varadharajan <sarangapani.singapore@gmail.com>
2023-03-08 07:17:18 +0800

    Added New relic agent

--- src/main/java/com/saranganrajan/apps/coredomainextractor/extract/FileExtractor.java
+++ src/main/java/com/saranganrajan/apps/coredomainextractor/extract/FileExtractor.java
@@ -1,9 +1,6 @@
 package com.saranganrajan.apps.coredomainextractor.extract;
 
-import com.saranganrajan.apps.coredomainextractor.model.PolicyTransaction;
-
 import java.io.IOException;
-import java.util.List;
 
 public interface FileExtractor {
     public void extractRawData() throws IOException;
